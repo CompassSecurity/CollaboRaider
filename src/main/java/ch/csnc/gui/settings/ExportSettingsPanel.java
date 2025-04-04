efd908f00f19da16da0f276c7b27b8cd5b61e25e
dec1m0s <andreas.brombach@compass-security.com>
2025-04-04 20:25:11 +0200

    moved file chooser to separate class

--- src/main/java/ch/csnc/gui/settings/ExportSettingsPanel.java
+++ src/main/java/ch/csnc/gui/settings/ExportSettingsPanel.java
@@ -1,25 +1,33 @@
 package ch.csnc.gui.settings;
 
 import ch.csnc.gui.GBC;
+import ch.csnc.gui.components.FileChooser;
+import ch.csnc.settings.SettingsModel;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class ExportSettingsPanel extends SettingsPanel {
+public class ExportSettingsPanel extends AbstractSettingsPanel {
     JLabel exportLabel, importlabel;
+    FileChooser fileChooser;
 
-    public ExportSettingsPanel() {
+    public ExportSettingsPanel(SettingsModel settingsModel) {
         super("Import / Export Settings");
 
         // Create buttons
         JButton importButton = new JButton("Import settings from file");
         JButton exportButton = new JButton("Export current settings to file");
+
+        // Initialize file chooser
+        fileChooser = new FileChooser(this);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setSelectedFile(new File("collaboraider_export.json"));
+        fileChooser.setFileFilter(new FileNameExtensionFilter("json file", "json"));
 
         // Add listeners for buttons
         exportButton.addActionListener(this::clickExportButton);
@@ -47,11 +55,9 @@ public class ExportSettingsPanel extends SettingsPanel {
     }
 
     private void clickExportButton(ActionEvent e) {
-        JFileChooser jFileChooser = getJFileChooser();
-
-        int returnVal = jFileChooser.showSaveDialog(this);
+        int returnVal = fileChooser.showSaveDialog(this);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
-            File file = jFileChooser.getSelectedFile();
+            File file = fileChooser.getSelectedFile();
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
                 writer.write("Hello from Burp");
                 exportLabel.setText("Exported to " + file.getAbsolutePath());
@@ -64,46 +70,11 @@ public class ExportSettingsPanel extends SettingsPanel {
         }
     }
 
-    /**
-     * Extend default chooser with confirmation dialog if the file already exists
-     *
-     * @return JFileChooser
-     */
-    private JFileChooser getJFileChooser() {
-        JFileChooser jFileChooser = new JFileChooser() {
-            @Override
-            public void approveSelection() {
-                File f = getSelectedFile();
-                if (getDialogType() == SAVE_DIALOG && f.exists()) {
-                    int result = JOptionPane.showConfirmDialog(this,
-                                                               "This file already exists. Do you want to overwrite it?",
-                                                               "Overwrite existing file?",
-                                                               JOptionPane.YES_NO_CANCEL_OPTION);
-                    if (result == JOptionPane.YES_OPTION) {
-                        super.approveSelection();
-                    } else {
-                        super.cancelSelection();
-                    }
-                } else {
-                    super.approveSelection();
-                }
-            }
-        };
-
-        // Add filter to only show JSON files
-        jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        jFileChooser.setSelectedFile(new File("collaboraider_export.json"));
-        jFileChooser.setFileFilter(new FileNameExtensionFilter("json file", "json"));
-
-        return jFileChooser;
-    }
-
     private void clickImportButton(ActionEvent e) {
-        JFileChooser jFileChooser = getJFileChooser();
-        int returnVal = jFileChooser.showOpenDialog(this);
+        int returnVal = fileChooser.showOpenDialog(this);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
-            File file = jFileChooser.getSelectedFile();
-            importlabel.setText("Imported " + file.getAbsolutePath());
+            File file = fileChooser.getSelectedFile();
+            importlabel.setText("Imported from file %s".formatted(file.getAbsolutePath()));
         }
     }
 }
