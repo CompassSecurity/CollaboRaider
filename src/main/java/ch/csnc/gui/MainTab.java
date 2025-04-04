37ee0e71d9535e472b756b9c508244daabc48bc1
dec1m0s <andreas.brombach@compass-security.com>
2025-04-04 20:25:28 +0200

    use separate settings class

--- src/main/java/ch/csnc/gui/MainTab.java
+++ src/main/java/ch/csnc/gui/MainTab.java
@@ -1,31 +1,26 @@
 package ch.csnc.gui;
 
 import burp.api.montoya.MontoyaApi;
-import ch.csnc.gui.interactions.InteractionsTab;
 import ch.csnc.interaction.PingbackTableModel;
 import ch.csnc.payload.PayloadsTableModel;
+import ch.csnc.settings.SettingsModel;
 
 import javax.swing.*;
 
 public class MainTab extends JTabbedPane {
-    public MainTab(MontoyaApi montoyaApi, PingbackTableModel pingbackTableModel, PayloadsTableModel settingsTableModel) {
+    public MainTab(MontoyaApi montoyaApi,
+                   PingbackTableModel pingbackTableModel,
+                   PayloadsTableModel settingsTableModel,
+                   SettingsModel settingsModel) {
         super();
-        InteractionsTab interactionsTab = new InteractionsTab(montoyaApi, pingbackTableModel);
-        PayloadsTab payloadsTab = new PayloadsTab(montoyaApi, settingsTableModel);
-        SettingsTab settingsTab = new SettingsTab();
 
+        InteractionsTab interactionsTab = new InteractionsTab(montoyaApi, pingbackTableModel, settingsModel);
+        PayloadsTab payloadsTab = new PayloadsTab(montoyaApi, settingsTableModel);
+        SettingsTab settingsTab = new SettingsTab(settingsModel);
+
+        this.addTab(settingsTab.TAB_TITLE, new JScrollPane(settingsTab));
         this.addTab(interactionsTab.TAB_TITLE, interactionsTab);
         this.addTab(payloadsTab.TAB_TITLE, payloadsTab);
-        this.addTab(settingsTab.TAB_TITLE, new JScrollPane(settingsTab));
 
-
-        /*
-        addChangeListener(e -> {
-            montoyaApi.logging().logToOutput("Current tab: " + this.getSelectedComponent().getClass().getSimpleName());
-            if (this.getSelectedComponent() instanceof InteractionsTab) {
-                interactionsTab.refresh();
-            }
-        });
-        */
     }
 }
