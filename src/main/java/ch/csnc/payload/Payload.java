1618a301283877fb4f4539bb5d7d2dccc25801f1
dec1m0s <andreas.brombach@compass-security.com>
2025-04-07 06:08:32 +0200

    serialize payloads

--- src/main/java/ch/csnc/payload/Payload.java
+++ src/main/java/ch/csnc/payload/Payload.java
@@ -1,10 +1,17 @@
 package ch.csnc.payload;
 
-public class Payload {
+import burp.api.montoya.persistence.PersistedObject;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Payload implements Serializable {
     public Boolean isActive;
     public PayloadType type;
     public String key;
     public String value;
+
+    private static final String separator = "#";
 
     public Payload(Boolean isActive, PayloadType type, String key, String value) {
         this.isActive = isActive;
@@ -43,5 +50,33 @@ public class Payload {
 
     public void setActive(Boolean active) {
         isActive = active;
+    }
+
+    /**
+     * Provide a way to serialize into a String so that it can be stored to persistent preferences
+     * @return
+     */
+    public String toString() {
+        String out = "";
+        out += (isActive) ? "1" : "0";
+        out += separator;
+        out += type.name();
+        out += separator;
+        out += key;
+        out += separator;
+        out += value;
+
+        assert out.split(separator).length == 4;
+
+        return out;
+    }
+
+    public static Payload fromString(String serialized) {
+        String[] components = serialized.split(separator);
+        assert components.length == 4;
+
+        boolean isActive = Objects.equals(components[0], "1");
+        PayloadType payloadType = PayloadType.valueOf(components[1]);
+        return new Payload(isActive, payloadType, components[2], components[3]);
     }
 }
