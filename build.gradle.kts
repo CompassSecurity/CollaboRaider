2f8a05ee3016f8857060567fc74f51fd516894cb
dec1m0s <andreas.brombach@compass-security.com>
2025-04-23 06:07:50 +0200

    go back to kotlin gradle

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+plugins {
+    id("java")
+}
+
+version = "2.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("net.portswigger.burp.extensions:montoya-api:2025.2")
+}
+
+tasks.register<Task>("captureBuildTime") {
+    doLast {
+        val buildTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd,HH:mm:ss"))
+        file("build-time.properties").writeText(
+            "" +
+                    "build.version=$version\n" +
+                    "build.time=$buildTime"
+        )
+    }
+}
+
+tasks.jar {
+    dependsOn(tasks["captureBuildTime"])
+    from(file("build-time.properties"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
