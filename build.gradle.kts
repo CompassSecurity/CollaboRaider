505d74ef3446d8fa7bd22c99099c084eae43cf8b
dec1m0s <andreas.brombach@compass-security.com>
2025-04-25 06:10:45 +0200

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,13 @@ plugins {
     id("java")
 }
 
-version = "2.0.0"
+version = "2.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     compileOnly("net.portswigger.burp.extensions:montoya-api:2025.2")
 }
 
