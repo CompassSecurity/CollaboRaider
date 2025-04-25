7096e0353caa152157fdc33b3a42b76efe456331
dec1m0s <andreas.brombach@compass-security.com>
2025-04-25 08:49:52 +0200

    cleanup build.gradle.kts, add file build-time.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+// Specify current version of the extension
+version = "2.0.1"
+
 plugins {
     id("java")
 }
 
-version = "2.0.1"
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
 
 repositories {
     mavenCentral()
@@ -15,19 +21,16 @@ dependencies {
     compileOnly("net.portswigger.burp.extensions:montoya-api:2025.2")
 }
 
-tasks.register<Task>("captureBuildTime") {
-    doLast {
-        val buildTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd,HH:mm:ss"))
-        file("build-time.properties").writeText(
-            "" +
-                    "build.version=$version\n" +
-                    "build.time=$buildTime"
-        )
-    }
-}
-
 tasks.jar {
-    dependsOn(tasks["captureBuildTime"])
+    // Add current time and version to file "build-time.properties"
+    val buildTime = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now())
+
+    val output = "build.version=$version\n" +
+                 "build.time=$buildTime\n" +
+                 "build.gradle=${gradle.gradleVersion}\n" +
+                 "build.jdk=${System.getProperty("java.version")}\n"
+
+    file("build-time.properties").writeText(output)
     from(file("build-time.properties"))
 }
 
