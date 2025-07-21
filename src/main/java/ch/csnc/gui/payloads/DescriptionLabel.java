e250e3f9eca166b6c842eabc24c66c5f6ced459a
dec1m0s <andreas.brombach@compass-security.com>
2025-07-21 17:18:24 +0200

    show example payload in description

--- src/main/java/ch/csnc/gui/payloads/DescriptionLabel.java
+++ src/main/java/ch/csnc/gui/payloads/DescriptionLabel.java
@@ -1,9 +1,11 @@
 package ch.csnc.gui.payloads;
 
+import ch.csnc.settings.SettingsModel;
+
 import javax.swing.*;
 
 public class DescriptionLabel extends JLabel {
-    public DescriptionLabel() {
+    public DescriptionLabel(SettingsModel settingsModel) {
         setHorizontalAlignment(SwingConstants.LEFT);
         setVerticalAlignment(SwingConstants.TOP);
 
@@ -15,12 +17,12 @@ public class DescriptionLabel extends JLabel {
                 <html>
                 Use placeholders to define payloads:
                 <ul>
-                <li><b><tt>%s</tt></b> will be replaced with a randomly generated Collaborator URL.</li>
-                <li><b><tt>%h</tt></b> will be replaced with the <b>host</b> of the current request.</li>
-                <li><b><tt>%o</tt></b> will be replaced with the <b>origin</b> of the current request.</li>
-                <li><b><tt>%r</tt></b> will be replaced with the <b>referer</b> of the current request.</li>
+                <li><b><tt>%%s</tt></b> will be replaced with a randomly generated Collaborator URL, eg. <tt>%s</tt>.</li>
+                <li><b><tt>%%h</tt></b> will be replaced with the <b>Host</b> header value of the current request.</li>
+                <li><b><tt>%%o</tt></b> will be replaced with the <b>Origin</b> header value of the current request.</li>
+                <li><b><tt>%%r</tt></b> will be replaced with the <b>Referer</b> header value of the current request.</li>
                 </ul>
                 </html>
-                """);
+                """.formatted(settingsModel.getCheckIpPayload()));
     }
 }
