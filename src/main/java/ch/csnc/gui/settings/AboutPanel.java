def55c4d2dfa5de147aa4009be7eac8d1c1aa465
dec1m0s <andreas.brombach@compass-security.com>
2025-07-30 15:29:21 +0200

    make links clickable, because why not

--- src/main/java/ch/csnc/gui/settings/AboutPanel.java
+++ src/main/java/ch/csnc/gui/settings/AboutPanel.java
@@ -5,6 +5,10 @@ import ch.csnc.gui.GBC;
 import ch.csnc.settings.SettingsModel;
 
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class AboutPanel extends AbstractSettingsPanel {
     public AboutPanel(SettingsModel settingsModel) {
@@ -28,9 +32,24 @@ public class AboutPanel extends AbstractSettingsPanel {
                     .setWeights(0, 1)
                     .setMargin(0, leftMargin, bottomMarginInGroup, 0));
 
-        JLabel developerLabel = new JLabel();
+        JEditorPane developerLabel = new JEditorPane();
         developerLabel.putClientProperty("html.disable", null);
         developerLabel.setOpaque(false);
+        developerLabel.setContentType("text/html");
+        developerLabel.setEditable(false);
+        developerLabel.setFocusable(false);
+        developerLabel.setBackground(UIManager.getColor("Label.background"));
+        developerLabel.setBorder(UIManager.getBorder("Label.border"));
+        // Add listener to open links
+        developerLabel.addHyperlinkListener(hle -> {
+            if (HyperlinkEvent.EventType.ACTIVATED.equals(hle.getEventType())) {
+                try {
+                    Desktop.getDesktop().browse(hle.getURL().toURI());
+                } catch (IOException | URISyntaxException e) {
+                    // ignore
+                }
+            }
+        });
         developerLabel.setText("""
                                <html>
                                Developed by Andreas Brombach @ Compass Security (<a href="https://www.compass-security.com">https://compass-security.com</a>), <br>
